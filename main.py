016725e476ee0ac30d27d191168943b3a1357491
shining <tderkkwii+social@gmail.com>
2025-03-03 16:03:50 +0900

    可能な指し手の表示を実装

---
+++ main.py
@@ -0,0 +1,495 @@
+import sys
+from typing import Generator
+from piece_types import (
+    PieceTypes,
+    Piece,
+    Color,
+    PIECE_TO_STR,
+    CHAR_TO_PIECE,
+    NON_PROMOTED_TO_PROMOTED,
+)
+
+
+def as_promoted(piece: Piece) -> Piece:
+    """成り駒に変換する（成れる場合のみ変換）"""
+    return NON_PROMOTED_TO_PROMOTED.get(piece, piece)
+
+
+def to_color(piece):
+    """駒の色を判定する"""
+    if piece == Piece.NO_PIECE:
+        return None  # 無駄な計算を避ける
+    return Color.BLACK if piece < 15 else Color.WHITE
+
+
+class Position:
+    start_position_sfen = (
+        "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1"
+    )
+    BOARD_SIZE = 9  # 盤面の一辺のマス数
+
+    def __init__(self):
+        self.side_to_move = Color.BLACK  # 初期手番は先手
+        self.board = [
+            [Piece.NO_PIECE.value] * self.BOARD_SIZE for _ in range(self.BOARD_SIZE)
+        ]  # 盤面初期化
+        self.hand_piece = [0] * len(Piece)  # 持ち駒の初期化
+        self.play = 1  # 初期手数
+
+    def __str__(self):
+        result = []
+
+        # ボードの状態を表示
+        result.append("+----+----+----+----+----+----+----+----+----+\n")
+        for rank in range(self.BOARD_SIZE):
+            result.append("|")
+            for file in range(self.BOARD_SIZE - 1, -1, -1):
+                result.append(self.to_human_readable_string(self.board[file][rank]))
+                result.append("|")
+            result.append("\n")
+            result.append("+----+----+----+----+----+----+----+----+----+\n")
+
+        # 持ち駒の状態を表示
+        result.append("先手 手駒: ")
+        for piece in range(Piece.BLACK_PAWN.value, Piece.WHITE_PAWN.value):
+            for _ in range(self.hand_piece[piece]):
+                result.append(self.to_human_readable_string(piece).strip()[0])
+                # result.append(self.to_human_readable_string(Piece(piece)).strip()[0])
+
+        result.append(" , 後手 手駒: ")
+        for piece in range(Piece.WHITE_PAWN.value, Piece.NUM_PIECES.value):
+            for _ in range(self.hand_piece[piece]):
+                result.append(self.to_human_readable_string(piece).strip()[0])
+                # result.append(self.to_human_readable_string(Piece(piece)).strip()[0])
+
+        result.append("\n")
+
+        # 手番を表示
+        result.append(
+            f"手番 = {'先手' if self.side_to_move == Color.BLACK else '後手'}\n"
+        )
+        return "".join(result)
+
+    def to_human_readable_string(self, piece):
+        if not 0 <= piece < len(PIECE_TO_STR):
+            return " "
+        return PIECE_TO_STR[piece]
+
+    def set_position(self, sfen):
+        self.side_to_move = Color.BLACK
+        self.board = [
+            [Piece.NO_PIECE.value] * self.BOARD_SIZE for _ in range(self.BOARD_SIZE)
+        ]
+        self.hand_piece = [0] * len(Piece)
+        self.play = 1
+
+        file = self.BOARD_SIZE - 1
+        rank = 0
+        index = 0
+        promotion = False
+
+        # 盤面のパース
+        while index < len(sfen):
+            c = sfen[index]
+            index += 1
+            if c == " ":
+                break
+            elif c == "/":
+                file = self.BOARD_SIZE - 1
+                rank += 1
+            elif c == "+":
+                promotion = True
+            elif c.isdigit():
+                empty_sequnce = int(c)
+                for _ in range(empty_sequnce):
+                    self.board[file][rank] = Piece.NO_PIECE.value
+                    file -= 1
+            else:
+                piece = CHAR_TO_PIECE.get(c)
+                assert piece is not None
+                if promotion:
+                    piece = as_promoted(piece)
+                    promotion = False
+                self.board[file][rank] = piece.value
+                file -= 1
+
+        # 手番のパース
+        if sfen[index] == "b":
+            self.side_to_move = Color.BLACK
+        else:
+            self.side_to_move = Color.WHITE
+        index += 2
+
+        count = 0
+        # 持ち駒のパース
+        while index < len(sfen):
+            if sfen[index] == " ":
+                break
+            c = sfen[index]
+            index += 1
+            if c == " ":
+                break
+            if c == "-":
+                continue
+            if c.isdigit():
+                count = count * 10 + int(c)
+                continue
+            piece = CHAR_TO_PIECE.get(c)
+            assert piece is not None
+            self.hand_piece[piece.value] = max(1, count)
+            count = 0
+
+        # 手数のパース
+        index += 1
+        self.play = int(sfen[index:])
+
+
+class Move:
+    RankToKanjiLetters = ["一", "二", "三", "四", "五", "六", "七", "八", "九"]
+    ToHumanReadableString = ["☗", "☖"]
+    # 静的インスタンス
+    Resign = None
+    Win = None
+    NoneMove = None
+
+    def __init__(
+        self,
+        file_from,
+        rank_from=None,
+        piece_from=None,
+        file_to=None,
+        rank_to=None,
+        piece_to=None,
+        drop=False,
+        promotion=False,
+        side_to_move=None,
+    ):
+        self.file_from = file_from
+        self.rank_from = rank_from
+        self.piece_from = piece_from
+        self.file_to = file_to
+        self.rank_to = rank_to
+        self.piece_to = piece_to
+        self.drop = drop
+        self.promotion = promotion
+        self.side_to_move = side_to_move
+
+    def __str__(self):
+        """指し手の文字列変換(デバッグ用)"""
+        return (
+            f"{self.ToHumanReadableString[self.side_to_move.value]} "
+            f"{chr(ord('１') + self.file_to)}"
+            f"{self.RankToKanjiLetters[self.rank_to]}"
+            f"{PIECE_TO_STR[self.piece_from]}{"成" if self.promotion else "  "}"
+        )
+
+    def __eq__(self, other):
+        # 比較関数
+        if not isinstance(other, Move):
+            return False
+        return (
+            self.file_from == other.file_from
+            and self.rank_from == other.rank_from
+            and self.piece_from == other.piece_from
+            and self.file_to == other.file_to
+            and self.rank_to == other.rank_to
+            and self.piece_to == other.piece_to
+            and self.drop == other.drop
+            and self.promotion == other.promotion
+            and self.side_to_move == other.side_to_move
+        )
+
+    def __hash__(self):
+        # ハッシュ関数
+        seed = 0
+        seed ^= self.file_from + ((seed << 6) + (seed >> 2))
+        seed ^= self.rank_from + ((seed << 6) + (seed >> 2))
+        seed ^= hash(self.piece_from) + ((seed << 6) + (seed >> 2))
+        seed ^= self.file_to + ((seed << 6) + (seed >> 2))
+        seed ^= self.rank_to + ((seed << 6) + (seed >> 2))
+        seed ^= hash(self.piece_to) + ((seed << 6) + (seed >> 2))
+        seed ^= int(self.drop) + ((seed << 6) + (seed >> 2))
+        seed ^= int(self.promotion) + ((seed << 6) + (seed >> 2))
+        seed ^= hash(self.side_to_move) + ((seed << 6) + (seed >> 2))
+        return seed
+
+    def to_usi_string(self):
+        if self == Move.Resign:
+            return "resign"
+        elif self == Move.Win:
+            return "win"
+
+        usi_string = ""
+        if self.drop:
+            usi_string += self.piece_from.to_usi_char().upper()
+            usi_string += "*"
+        else:
+            usi_string += chr(self.file_from + ord("1"))
+            usi_string += chr(self.rank_from + ord("a"))
+
+        usi_string += chr(self.file_to + ord("1"))
+        usi_string += chr(self.rank_to + ord("a"))
+
+        if self.promotion:
+            usi_string += "+"
+
+        return usi_string
+
+    @staticmethod
+    def from_usi_string(position, move_string):
+        if move_string == "resign":
+            return Move.Resign
+        elif move_string == "win":
+            return Move.Win
+        elif move_string == "none":
+            return Move.NoneMove
+
+        move = Move(
+            file_from=-1,
+            rank_from=-1,
+            piece_from=None,
+            file_to=None,
+            rank_to=None,
+            piece_to=None,
+        )
+        if move_string[1] == "*":
+            # 駒打ちの指し手
+            move.file_from = -1
+            move.rank_from = -1
+            move.piece_from = CHAR_TO_PIECE[move_string[0]]
+            if position.side_to_move == Color.White:
+                move.piece_from = move.piece_from.as_opponent_hand_piece()
+            move.drop = True
+        else:
+            # 駒を移動する指し手
+            move.file_from = ord(move_string[0]) - ord("1")
+            move.rank_from = ord(move_string[1]) - ord("a")
+            move.piece_from = position.board[move.file_from][move.rank_from]
+            move.drop = False
+
+        move.file_to = ord(move_string[2]) - ord("1")
+        move.rank_to = ord(move_string[3]) - ord("a")
+        move.piece_to = position.board[move.file_to][move.rank_to]
+
+        move.promotion = len(move_string) == 5
+        move.side_to_move = position.side_to_move
+        return move
+
+
+# 静的なインスタンス定義
+Move.Resign = Move(file_from=2, file_to=2)
+Move.Win = Move(file_from=3, file_to=3)
+Move.NoneMove = Move(file_from=4, file_to=4)
+
+
+def can_promote(piece):
+    return piece in NON_PROMOTED_TO_PROMOTED
+
+
+def can_put_without_promotion(piece_from: Piece, rank_to: int) -> bool:
+    return (
+        (piece_from == Piece.BLACK_PAWN.value and rank_to >= 1)
+        or (piece_from == Piece.BLACK_LANCE.value and rank_to >= 1)
+        or (piece_from == Piece.BLACK_KNIGHT.value and rank_to >= 2)
+        or (piece_from == Piece.WHITE_PAWN.value and rank_to <= 7)
+        or (piece_from == Piece.WHITE_LANCE.value and rank_to <= 7)
+        or (piece_from == Piece.WHITE_KNIGHT.value and rank_to <= 6)
+        or (
+            piece_from
+            not in {
+                Piece.BLACK_PAWN.value,
+                Piece.BLACK_LANCE.value,
+                Piece.BLACK_KNIGHT.value,
+                Piece.WHITE_PAWN.value,
+                Piece.WHITE_LANCE.value,
+                Piece.WHITE_KNIGHT.value,
+            }
+        )
+    )
+
+
+def is_pawn_exist(board, file, pawn) -> bool:
+    for rank in range(Position.BOARD_SIZE):
+        if board[file][rank] == pawn:
+            return True
+    return False
+
+
+# 指し手生成関数
+def generate(position) -> Generator[Move, None, None]:
+    side_to_move = position.side_to_move
+    board = position.board
+    hand_pieces = position.hand_piece
+    non_capture_promotion_moves = []
+    non_capture_non_promotion_moves = []
+
+    # 駒を移動する指し手
+    for file_from in range(Position.BOARD_SIZE):
+        for rank_from in range(Position.BOARD_SIZE):
+            piece_from = board[file_from][rank_from]
+            if piece_from == Piece.NO_PIECE.value:
+                continue
+            if to_color(piece_from) != side_to_move:
+                continue
+
+            for move_direction in PieceTypes.MoveDirections[int(piece_from)]:
+                max_distance = 8 if move_direction.is_long else 1
+                file_to = file_from
+                rank_to = rank_from
+                for distance in range(max_distance):
+                    file_to += move_direction.direction.delta_file
+                    rank_to += move_direction.direction.delta_rank
+
+                    if not (
+                        0 <= file_to < Position.BOARD_SIZE
+                        and 0 <= rank_to < Position.BOARD_SIZE
+                    ):
+                        continue
+
+                    piece_to = board[file_to][rank_to]
+                    if (
+                        piece_to != Piece.NO_PIECE.value
+                        and to_color(piece_to) == side_to_move
+                    ):
+                        break
+
+                    if can_promote(piece_from) and (
+                        (side_to_move == Color.BLACK and rank_to <= 2)
+                        or (side_to_move == Color.WHITE and rank_to >= 6)
+                        or (side_to_move == Color.BLACK and rank_from <= 2)
+                        or (side_to_move == Color.WHITE and rank_from >= 6)
+                    ):
+                        move = Move(
+                            file_from,
+                            rank_from,
+                            piece_from,
+                            file_to,
+                            rank_to,
+                            piece_to,
+                            False,
+                            True,
+                            side_to_move,
+                        )
+
+                        if move.piece_to != Piece.NO_PIECE.value:
+                            yield move
+                        else:
+                            non_capture_promotion_moves.append(move)
+
+                    if can_put_without_promotion(piece_from, rank_to):
+                        move = Move(
+                            file_from,
+                            rank_from,
+                            piece_from,
+                            file_to,
+                            rank_to,
+                            piece_to,
+                            False,
+                            False,
+                            side_to_move,
+                        )
+
+                        if move.piece_to != Piece.NO_PIECE.value:
+                            yield move
+                        else:
+                            non_capture_non_promotion_moves.append(move)
+
+                    if piece_to != Piece.NO_PIECE.value:
+                        break
+
+    # 駒を取らない、成る指し手
+    for move in non_capture_promotion_moves:
+        yield move
+
+    # 駒を取らない、成らない指し手
+    for move in non_capture_non_promotion_moves:
+        yield move
+
+    # 駒を打つ指し手
+    min_piece = (
+        Piece.BLACK_PAWN.value
+        if side_to_move == Color.BLACK
+        else Piece.WHITE_PAWN.value
+    )
+    max_piece = (
+        Piece.BLACK_ROOK.value
+        if side_to_move == Color.BLACK
+        else Piece.WHITE_ROOK.value
+    )
+    for piece_from in range(min_piece, max_piece + 1):
+        if hand_pieces[piece_from] == 0:
+            continue
+
+        for file_to in range(Position.BOARD_SIZE):
+            for rank_to in range(Position.BOARD_SIZE):
+                if board[file_to][rank_to] != Piece.NO_PIECE.value:
+                    continue
+
+                if not can_put_without_promotion(piece_from, rank_to):
+                    continue
+
+                if (
+                    piece_from == Piece.BLACK_PAWN or piece_from == Piece.WHITE_PAWN
+                ) and is_pawn_exist(board, file_to, piece_from):
+                    continue
+
+                yield Move(
+                    -1,
+                    -1,
+                    piece_from,
+                    file_to,
+                    rank_to,
+                    Piece.NO_PIECE.value,
+                    True,
+                    False,
+                    side_to_move,
+                )
+
+
+def main():
+    position = Position()
+    while True:
+        line = sys.stdin.readline().strip()
+        if not line:
+            continue
+
+        command = line.split()[0]
+        match command:
+            case "usi":
+                print("id name SimpleShogiEngine")
+                print("id author YourName")
+                print("usiok")
+                sys.stdout.flush()
+            case "isready":
+                print("readyok")
+                sys.stdout.flush()
+            case "position":
+                assert len(line.split()) >= 2
+                if line.split()[1] == "sfen":
+                    position.set_position(" ".join(line.split()[2:]))
+                    next_index = 6
+                elif line.split()[1] == "startpos":
+                    position.set_position(Position.start_position_sfen)
+                    next_index = 2
+                else:
+                    print("不正なコマンドです")
+            case "generatemove":
+                count = 0
+                for move in generate(position):
+                    print(move)
+                    sys.stdout.flush()
+                    count += 1
+                print(f"合計 {count} 通り")
+                break
+            case "go":
+                print("bestmove resign")
+                sys.stdout.flush()
+            case "quit":
+                break
+            case "d":
+                print(position)
+                sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    main()
---
+++ piece_types.py
@@ -0,0 +1,364 @@
+from enum import Enum
+
+
+class Color(Enum):
+    BLACK = 0
+    WHITE = 1
+    NUM_COLORS = 2
+
+
+class Piece(Enum):
+    NO_PIECE = 0
+    BLACK_PAWN = 1
+    BLACK_LANCE = 2
+    BLACK_KNIGHT = 3
+    BLACK_SILVER = 4
+    BLACK_GOLD = 5
+    BLACK_BISHOP = 6
+    BLACK_ROOK = 7
+    BLACK_KING = 8
+    BLACK_PROMOTED_PAWN = 9
+    BLACK_PROMOTED_LANCE = 10
+    BLACK_PROMOTED_KNIGHT = 11
+    BLACK_PROMOTED_SILVER = 12
+    BLACK_HORSE = 13
+    BLACK_DRAGON = 14
+    WHITE_PAWN = 15
+    WHITE_LANCE = 16
+    WHITE_KNIGHT = 17
+    WHITE_SILVER = 18
+    WHITE_GOLD = 19
+    WHITE_BISHOP = 20
+    WHITE_ROOK = 21
+    WHITE_KING = 22
+    WHITE_PROMOTED_PAWN = 23
+    WHITE_PROMOTED_LANCE = 24
+    WHITE_PROMOTED_KNIGHT = 25
+    WHITE_PROMOTED_SILVER = 26
+    WHITE_HORSE = 27
+    WHITE_DRAGON = 28
+    NUM_PIECES = 29
+
+
+PIECE_TO_STR = [
+    "    ",
+    " 歩 ",
+    " 香 ",
+    " 桂 ",
+    " 銀 ",
+    " 金 ",
+    " 角 ",
+    " 飛 ",
+    " 王 ",
+    " と ",
+    " 杏 ",
+    " 圭 ",
+    " 全 ",
+    " 馬 ",
+    " 龍 ",
+    " 歩↓",
+    " 香↓",
+    " 桂↓",
+    " 銀↓",
+    " 金↓",
+    " 角↓",
+    " 飛↓",
+    " 王↓",
+    " と↓",
+    " 杏↓",
+    " 圭↓",
+    " 全↓",
+    " 馬↓",
+    " 龍↓",
+    None,
+]
+
+CHAR_TO_PIECE = {
+    "K": Piece.BLACK_KING,
+    "k": Piece.WHITE_KING,
+    "R": Piece.BLACK_ROOK,
+    "r": Piece.WHITE_ROOK,
+    "B": Piece.BLACK_BISHOP,
+    "b": Piece.WHITE_BISHOP,
+    "G": Piece.BLACK_GOLD,
+    "g": Piece.WHITE_GOLD,
+    "S": Piece.BLACK_SILVER,
+    "s": Piece.WHITE_SILVER,
+    "N": Piece.BLACK_KNIGHT,
+    "n": Piece.WHITE_KNIGHT,
+    "L": Piece.BLACK_LANCE,
+    "l": Piece.WHITE_LANCE,
+    "P": Piece.BLACK_PAWN,
+    "p": Piece.WHITE_PAWN,
+}
+
+NON_PROMOTED_TO_PROMOTED = {
+    Piece.BLACK_PAWN.value: Piece.BLACK_PROMOTED_PAWN,
+    Piece.BLACK_LANCE.value: Piece.BLACK_PROMOTED_LANCE,
+    Piece.BLACK_KNIGHT.value: Piece.BLACK_PROMOTED_KNIGHT,
+    Piece.BLACK_SILVER.value: Piece.BLACK_PROMOTED_SILVER,
+    Piece.BLACK_BISHOP.value: Piece.BLACK_HORSE,
+    Piece.BLACK_ROOK.value: Piece.BLACK_DRAGON,
+    Piece.WHITE_PAWN.value: Piece.WHITE_PROMOTED_PAWN,
+    Piece.WHITE_LANCE.value: Piece.WHITE_PROMOTED_LANCE,
+    Piece.WHITE_KNIGHT.value: Piece.WHITE_PROMOTED_KNIGHT,
+    Piece.WHITE_SILVER.value: Piece.WHITE_PROMOTED_SILVER,
+    Piece.WHITE_BISHOP.value: Piece.WHITE_HORSE,
+    Piece.WHITE_ROOK.value: Piece.WHITE_DRAGON,
+}
+
+
+class MoveDirection:
+    def __init__(self, direction, is_long=False):
+        self.direction = direction
+        self.is_long = is_long
+
+
+class Direction:
+    def __init__(self, delta_file, delta_rank):
+        self.delta_file = delta_file
+        self.delta_rank = delta_rank
+
+
+class PieceTypes:
+    UpLeft = Direction(1, -1)
+    Up = Direction(0, -1)
+    UpRight = Direction(-1, -1)
+    Left = Direction(1, 0)
+    Right = Direction(-1, 0)
+    DownLeft = Direction(1, 1)
+    Down = Direction(0, 1)
+    DownRight = Direction(-1, 1)
+
+    MoveDirections = [
+        # NoPiece
+        None,
+        # BlackPawn
+        [
+            MoveDirection(Up),
+        ],
+        # BlackLance
+        [
+            MoveDirection(Up, True),
+        ],
+        # BlackKnight
+        [
+            MoveDirection(Direction(-1, -2)),
+            MoveDirection(Direction(1, -2)),
+        ],
+        # BlackSilver
+        [
+            MoveDirection(UpLeft),
+            MoveDirection(Up),
+            MoveDirection(UpRight),
+            MoveDirection(DownLeft),
+            MoveDirection(DownRight),
+        ],
+        # BlackGold
+        [
+            MoveDirection(UpLeft),
+            MoveDirection(Up),
+            MoveDirection(UpRight),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(Down),
+        ],
+        # BlackBishop
+        [
+            MoveDirection(UpLeft, True),
+            MoveDirection(UpRight, True),
+            MoveDirection(DownLeft, True),
+            MoveDirection(DownRight, True),
+        ],
+        # BlackRook
+        [
+            MoveDirection(Up, True),
+            MoveDirection(Left, True),
+            MoveDirection(Right, True),
+            MoveDirection(Down, True),
+        ],
+        # BlackKing
+        [
+            MoveDirection(UpLeft),
+            MoveDirection(Up),
+            MoveDirection(UpRight),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(DownLeft),
+            MoveDirection(Down),
+            MoveDirection(DownRight),
+        ],
+        # BlackPromotedPawn
+        [
+            MoveDirection(UpLeft),
+            MoveDirection(Up),
+            MoveDirection(UpRight),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(Down),
+        ],
+        # BlackPromotedLance
+        [
+            MoveDirection(UpLeft),
+            MoveDirection(Up),
+            MoveDirection(UpRight),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(Down),
+        ],
+        # BlackPromotedKnight
+        [
+            MoveDirection(UpLeft),
+            MoveDirection(Up),
+            MoveDirection(UpRight),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(Down),
+        ],
+        # BlackPromotedSilver
+        [
+            MoveDirection(UpLeft),
+            MoveDirection(Up),
+            MoveDirection(UpRight),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(Down),
+        ],
+        # BlackHorse
+        [
+            MoveDirection(UpLeft, True),
+            MoveDirection(Up),
+            MoveDirection(UpRight, True),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(DownLeft, True),
+            MoveDirection(Down),
+            MoveDirection(DownRight, True),
+        ],
+        # BlackDragon
+        [
+            MoveDirection(UpLeft),
+            MoveDirection(Up, True),
+            MoveDirection(UpRight),
+            MoveDirection(Left, True),
+            MoveDirection(Right, True),
+            MoveDirection(DownLeft),
+            MoveDirection(Down, True),
+            MoveDirection(DownRight),
+        ],
+        # WhitePawn
+        [
+            MoveDirection(Down),
+        ],
+        # WhiteLance
+        [
+            MoveDirection(Down, True),
+        ],
+        # WhiteKnight
+        [
+            MoveDirection(Direction(1, 2)),
+            MoveDirection(Direction(-1, 2)),
+        ],
+        # WhiteSilver
+        [
+            MoveDirection(UpLeft),
+            MoveDirection(UpRight),
+            MoveDirection(DownLeft),
+            MoveDirection(Down),
+            MoveDirection(DownRight),
+        ],
+        # WhiteGold
+        [
+            MoveDirection(Up),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(DownLeft),
+            MoveDirection(Down),
+            MoveDirection(DownRight),
+        ],
+        # WhiteBishop
+        [
+            MoveDirection(UpLeft, True),
+            MoveDirection(UpRight, True),
+            MoveDirection(DownLeft, True),
+            MoveDirection(DownRight, True),
+        ],
+        # WhiteRook
+        [
+            MoveDirection(Up, True),
+            MoveDirection(Left, True),
+            MoveDirection(Right, True),
+            MoveDirection(Down, True),
+        ],
+        # WhiteKing
+        [
+            MoveDirection(UpLeft),
+            MoveDirection(Up),
+            MoveDirection(UpRight),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(DownLeft),
+            MoveDirection(Down),
+            MoveDirection(DownRight),
+        ],
+        # WhitePromotedPawn
+        [
+            MoveDirection(Up),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(DownLeft),
+            MoveDirection(Down),
+            MoveDirection(DownRight),
+        ],
+        # WhitePromotedLance
+        [
+            MoveDirection(Up),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(DownLeft),
+            MoveDirection(Down),
+            MoveDirection(DownRight),
+        ],
+        # WhitePromotedKnight
+        [
+            MoveDirection(Up),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(DownLeft),
+            MoveDirection(Down),
+            MoveDirection(DownRight),
+        ],
+        # WhitePromotedSilver
+        [
+            MoveDirection(Up),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(DownLeft),
+            MoveDirection(Down),
+            MoveDirection(DownRight),
+        ],
+        # WhiteHorse
+        [
+            MoveDirection(UpLeft, True),
+            MoveDirection(Up),
+            MoveDirection(UpRight, True),
+            MoveDirection(Left),
+            MoveDirection(Right),
+            MoveDirection(DownLeft, True),
+            MoveDirection(Down),
+            MoveDirection(DownRight, True),
+        ],
+        # WhiteDragon
+        [
+            MoveDirection(UpLeft),
+            MoveDirection(Up, True),
+            MoveDirection(UpRight),
+            MoveDirection(Left, True),
+            MoveDirection(Right, True),
+            MoveDirection(DownLeft),
+            MoveDirection(Down, True),
+            MoveDirection(DownRight),
+        ],
+        # NumPieces
+        None,
+    ]
